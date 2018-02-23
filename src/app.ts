4b508486b298d228f9bf58cb5050242fc701865e
caiodsc <caio.dscamara@gmail.com>
2018-02-23 08:24:07 -0400

    mplementando primeira Query com GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as graphqlHTTP from "express-graphql";
+import schema from "./graphql/schema";
 
 class App {
     public express: express.Application;
@@ -9,12 +11,17 @@ class App {
     }
 
     private  middleware(): void{
-        this.express.use('/hello', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-            res.send({
-                hello: 'Hello World!'
-            });
-        });
+        // this.express.use('/hello', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        //     res.send({
+        //         hello: 'Hello World!'
+        //     });
+        // });
+
+        this.express.use('/graphql', graphqlHTTP({
+            schema: schema
+        }))
     }
 }
 
 export default new App().express;
+
